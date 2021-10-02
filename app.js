39c9cb0cb6fafa1af6b95faa5d38d523d10b21be
Renan Oliveira <reluiz@leroymerlin.com.br>
2021-10-02 13:40:05 -0300

    inserted userSchema and created authRoutes

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express()
 app.use(express.json());
 app.use(cors());
 
+//...import routes
+const authRoutes = require('./middlewares/auth.routes')
+
+//rotas
+
+app.use('auth',authRoutes)
+
 app.listen(process.env.PORT, ()=>{
     console.log(`server runing in port ${process.env.PORT}`)
 })
--- models/User-Schema.js
+++ models/User-Schema.js
@@ -1,1 +1,51 @@
-const {Model, Schema} = require('mongoose')
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        unique: true,
+        type: String,
+        required: true
+    },
+    passwordHash: {
+        required: true,
+        type: String
+    },
+    email: {
+        required: true,
+        type: String,
+        unique: true
+    },
+    biography: String,
+    posts: [
+
+    ],
+    following: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    followers: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    favoriteGames: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: ''
+        }
+    ]
+
+},
+    {
+        tymestamps: true
+    }
+);
+
+module.exports = model('User', userSchema)
