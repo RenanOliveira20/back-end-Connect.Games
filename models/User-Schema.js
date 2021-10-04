ab356c766369889613374b0b03a0ffc233df7643
Renan Oliveira <reluiz@leroymerlin.com.br>
2021-10-04 19:05:58 -0300

    auth middlewares switched and signup routes created

--- models/User-Schema.js
+++ models/User-Schema.js
@@ -1,48 +1,49 @@
 const { model, Schema } = require('mongoose');
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    username: {
-        unique: true,
-        type: String,
-        required: true
-    },
-    passwordHash: {
-        required: true,
-        type: String
-    },
-    email: {
-        required: true,
-        type: String,
-        unique: true
-    },
-    biography: String,
-    posts: [
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        username: {
+            unique: true,
+            type: String,
+            required: true
+        },
+        password: {
+            required: true,
+            type: String
+        },
+        email: {
+            required: true,
+            type: String,
+            unique: true
+        },
+        biography: String,
+        posts: [
 
-    ],
-    following: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ],
-    followers: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ],
-    favoriteGames: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: ''
-        }
-    ]
+        ],
+        following: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        followers: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        favoriteGames: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Game'
+            }
+        ]
 
-},
+    },
     {
         tymestamps: true
     }
