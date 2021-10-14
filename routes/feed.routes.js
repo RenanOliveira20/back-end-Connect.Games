c4fcf4483831c318e88903fd204dc5dec652ff86
Jhonatan Veras <jveras@lerotmerlin.com.br>
2021-10-13 22:27:07 -0300

    delete do post feito e funcionando

--- routes/feed.routes.js
+++ routes/feed.routes.js
@@ -1,31 +1,31 @@
 const { Router } = require("express");
 const User = require("../models/User.model");
 const Post = require("../models/Post.model");
+const Comment = require("../models/Comment.model.js");
 const router = Router();
-const { ObjectID } = require ("mongoose")
 
 //get all posts in chronological order
-router.get('/', async (req, res)=>{
-    const {id} = req.user
-    try {
-        const feed =[]
-        const user = await User.findOne({_id: id}).populate('following');
-        user.posts.forEach((e)=>{
-          feed.push(e)
-        })
-        user.following.forEach((e)=>{
-          e.posts.forEach((e)=>{
-            feed.push(e)
-          })
-        })
-        feed.sort( (a ,b) => { 
-        return a.createdAt - b.createdAt
-      })
-        res.status(200).json(feed)
-    } catch (error) {
-        console.log(error)
-    }
-})
+router.get("/", async (req, res) => {
+  const { id } = req.user;
+  try {
+    const feed = [];
+    const user = await User.findOne({ _id: id }).populate("following");
+    user.posts.forEach((e) => {
+      feed.push(e);
+    });
+    user.following.forEach((e) => {
+      e.posts.forEach((e) => {
+        feed.push(e);
+      });
+    });
+    feed.sort((a, b) => {
+      return a.createdAt - b.createdAt;
+    });
+    res.status(200).json(feed);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.put("/", async (req, res) => {
   const { text, imageUrl } = req.body;
@@ -37,7 +37,7 @@ router.put("/", async (req, res) => {
     const newPost = await Post.create({
       text,
       imageUrl,
-      userID: req.user.id,
+      user: req.user.id,
     });
     const userDb = await User.findOneAndUpdate(
       { _id: req.user.id },
@@ -51,18 +51,22 @@ router.put("/", async (req, res) => {
 });
 
 router.delete("/:idpost", async (req, res) => {
-  const { id } = req.user
+  const { id } = req.user;
   const { idpost } = req.params;
-  console.log(id)
+
   try {
-    await Post.findOneAndDelete({ _id: idpost, userID: id });
-    const user = await User.findById(id)
-    const index = user.posts.findIndex( element => element._id === idpost)
-    user.posts.splice(index, 1)
-    user.save()    
-    res.status(200).json({ message: `Post deleted` });
+    const deleteComments = await Post.findById(idpost);
+    deleteComments.comments.forEach(async (element) => {
+      await Comment.findOneAndDelete(element._id);
+    });
+    await Post.findOneAndDelete({ _id: idpost, user: id });
+    const user = await User.findById(id);
+    const index = user.posts.findIndex((element) => element._id === idpost);
+    user.posts.splice(index, 1);
+    user.save();
+    res.status(200).json({ message: `Post successfully deleted` });
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json(error);
   }
 });
 
