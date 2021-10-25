e82be1ec9f8341816bed580137c7abfe5906bdae
Jhonatan Veras <jveras@lerotmerlin.com.br>
2021-10-24 21:19:02 -0300

    alteraçao na rota de delete do post e feed

--- routes/feed.routes.js
+++ routes/feed.routes.js
@@ -7,38 +7,38 @@ const Comment = require("../models/Comment.model.js");
 const router = Router();
 
 //get all posts in chronological order
-router.get('/', async (req, res)=>{
-    const {id} = req.user
-    try {
-        const feed =[]
-        const user = await User.findOne({_id: id}).populate('following');
-        user.posts.forEach((e)=>{
-          feed.push(e)
-        })
-        user.following.forEach((e)=>{
-          e.posts.forEach((e)=>{
-            feed.push(e)
-          })
-        })
-        feed.sort( (a ,b) => { 
-        return a.createdAt - b.createdAt
-      })
-        res.status(200).json(feed)
-    } catch (error) {
-        res.status(500).json({
-          message: 'Error to get all posts to feed',
-          error: error.message
-        })
-    }
-})
+router.get("/", async (req, res) => {
+  const { id } = req.user;
+  try {
+    const feed = [];
+    const user = await User.findOne({ _id: id }).populate("following posts");
+    user.posts.forEach((e) => {
+      feed.push(e);
+    });
+    user.following.forEach((e) => {
+      e.posts.forEach((e) => {
+        feed.push(e);
+      });
+    });
+    feed.sort((a, b) => {
+      return a.createdAt - b.createdAt;
+    });
+    res.status(200).json(feed);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error to get all posts to feed",
+      error: error.message,
+    });
+  }
+});
 
 router.post("/", uploadImage.single("image"), async (req, res) => {
-  let path = ''
-  if (req.file){
-    path = req.file.path
+  let path = "";
+  if (req.file) {
+    path = req.file.path;
   }
   const { text } = req.body;
-  const imageUrl = path
+  const imageUrl = path;
 
   try {
     if (!text && !imageUrl) {
@@ -51,7 +51,7 @@ router.post("/", uploadImage.single("image"), async (req, res) => {
     });
     const userDb = await User.findOneAndUpdate(
       { _id: req.user.id },
-      { $push: { posts: newPost } },
+      { $push: { posts: newPost._id } },
       { new: true }
     );
     res.status(200).json({ message: "new post inserted" });
@@ -67,21 +67,24 @@ router.delete("/:idpost", async (req, res) => {
   try {
     const post = await Post.findById(idpost);
     const { imageUrl } = post;
-    const imgArray = imageUrl.split('/');
-    const img = imgArray[imgArray.length -1];
-    const imgName = img.split('.')[0]
+    const imgArray = imageUrl.split("/");
+    const img = imgArray[imgArray.length - 1];
+    const imgName = img.split(".")[0];
     await cloudinary.uploader.destroy(`ConnectGames/imagePost/${imgName}`);
     post.comments.forEach(async (element) => {
       await Comment.findOneAndDelete(element._id);
     });
     await Post.findOneAndDelete({ _id: idpost, user: id });
     const user = await User.findById(id);
-    const index = user.posts.findIndex((element) => element._id === idpost);
-    user.posts.splice(index, 1);
-    user.save();
-    res.status(200).json({ message: `Post successfully deleted` });
+    const index = user.posts.findIndex((element) => element == idpost);
+    if (index !== -1) {
+      user.posts.splice(index, 1);
+      user.save();  
+      return res.status(200).json({ message: `Post successfully deleted` });  
+    } 
+    res.status(400).json({ message: `post not found`})
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json(error.message);
   }
 });
 
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const ObjectID = require('mongoose')
+const ObjectID = require("mongoose");
 const Post = require("../models/Post.model");
 const Comment = require("../models/Comment.model");
 const User = require("../models/User.model");
@@ -14,7 +14,7 @@ router.get("/:id", async (req, res) => {
       throw new Error(`don't find a post`);
     }
     res.status(200).json(post);
-  } catch (error) { }
+  } catch (error) {}
 });
 
 router.post("/:id/comment", async (req, res) => {
@@ -99,7 +99,10 @@ router.put("/:id/reactionsComment", async (req, res) => {
     if (dislike) {
       const commentFromDb = await Comment.findById(id);
       if (commentFromDb.dislikes.includes(userID)) {
-        commentFromDb.dislikes.splice(commentFromDb.dislikes.indexOf(userID), 1);
+        commentFromDb.dislikes.splice(
+          commentFromDb.dislikes.indexOf(userID),
+          1
+        );
         commentFromDb.save();
         res.status(200).json(commentFromDb);
       } else {
@@ -116,31 +119,36 @@ router.put("/:id/reactionsComment", async (req, res) => {
 });
 
 //delete comment
-router.delete('/:id/:commentId', async (req, res) => {
+router.delete("/:id/:commentId", async (req, res) => {
   const { id, commentId } = req.params;
   const logUser = req.user.id;
   try {
-    const post = await Post.findOne({ _id: id }).populate('comments');
-    const comment = await Comment.findOne({ _id: commentId })
+    const post = await Post.findOne({ _id: id }).populate("comments");
+    const comment = await Comment.findOne({ _id: commentId });
     if (!comment) {
-      throw new Error(`Comment not find`)
+      throw new Error(`Comment not find`);
     }
     if (!post) {
-      throw new Error('Post not find')
+      throw new Error("Post not find");
     }
-    if (await Post.findOne({ $and: [{ _id: id }, { user: logUser }] }) || await Comment.findOne({ $and: [{ _id: id }, { user: logUser }] })) {
-      const index = post.comments.findIndex(e => e._id === commentId);
-      post.comments.splice(index, 1)
-      await Comment.findOneAndDelete({ _id: commentId })
-      res.status(200).json(`deleted a comment ${commentId}`)
+    if (
+      (await Post.findOne({ $and: [{ _id: id }, { user: logUser }] })) ||
+      (await Comment.findOne({ $and: [{ _id: id }, { user: logUser }] }))
+    ) {
+      const index = post.comments.findIndex((e) => e._id == commentId);
+      if (index !== -1) {
+        post.comments.splice(index, 1);
+        await Comment.findOneAndDelete({ _id: commentId });
+       return res.status(200).json(`deleted a comment ${commentId}`);
+      } 
+      res.status(400).json({message: 'comment not found'})
     }
-    throw new Error('unauthorized')
+    throw new Error("unauthorized");
   } catch (error) {
     res.status(500).json({
       message: "Error o delete the comment",
-      error: error.message
+      error: error.message,
     });
-  };
-}
-);
+  }
+});
 module.exports = router;
