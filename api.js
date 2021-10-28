0f7985184b6cc17f8bed5461ee830f9d1949575a
Luana <luana.nadai.santos@gmail.com>
2021-10-28 20:26:35 -0300

    Alteracao no banco de dados Games externo

--- api.js
+++ api.js
@@ -9,36 +9,39 @@ const migrate = async () => {
     const api = axios.create({
         baseURL: 'https://api.rawg.io/api/games?key=cbb5b86f21b641e194e2cf3dde368951'
     })
-    
+
     try {
         const result = await api.get(
             ""
         )
         console.log(result.data.results[0])
-    
+
         const myApi = axios.create({
             baseURL: 'http://localhost:5000/games'
         })
 
         const list = result.data.results
 
-        for (let i = 0; i < list.length ; i++) {
+        for (let i = 0; i < list.length; i++) {
             const game = list[i];
             // console.log(game)
-            delete game.id;
-            delete game.ratings;
-    
+            // delete game.id;
+            // delete game.ratings;
+            
             const resultGame = await myApi.post('', game);
             console.log(resultGame)
         };
-        
+
     } catch (error) {
         console.log(error)
-    } finally { 
+    } finally {
         console.log('--------------FINISH---------------')
     }
 
 
+
 }
 
+
 migrate()
+
